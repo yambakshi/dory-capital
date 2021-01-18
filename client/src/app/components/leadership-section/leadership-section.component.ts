caf27da0dd7fb466a1e49953f810e4dfd2ffe817
Yam Bakshi <yambakshi@gmail.com>
2021-01-18 18:39:08 +0200

    Improved Leadership Section

--- client/src/app/components/leadership-section/leadership-section.component.ts
+++ client/src/app/components/leadership-section/leadership-section.component.ts
@@ -10,170 +10,43 @@ import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@ang
     ]
 })
 export class LeadershipSectionComponent implements AfterViewInit {
+    @ViewChild('people') people: ElementRef;
     @ViewChild('skillsTypes') skillsTypes: ElementRef;
-    title: string = "BoD & Leadership";
+    title: string = "Leadership";
     skillsArray: any[] = [];
     media = {
         people: [
-            {
-                img: 'assets/media/leadership/people/17-reut-dory.png',
-                alt: 'Reut Dory',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/02-asaf-eli.png',
-                alt: 'Asaf Eli',
-                skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow']
-            },
-            {
-                img: 'assets/media/leadership/people/03-asher-dory.png',
-                alt: 'Asher Dory',
-                skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow']
-            },
-            {
-                img: 'assets/media/leadership/people/04-eli-clark.png',
-                alt: 'Eli Clark',
-                skills: ['life-sciences', 'ip', 'bd', 'hr', 'dealflow']
-            },
-            {
-                img: 'assets/media/leadership/people/17-reut-dory.png',
-                alt: 'Reut Dory',
-                skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow', 'sales']
-            },
-            {
-                img: 'assets/media/leadership/people/06-henry-chai.png',
-                alt: 'Henry Chai',
-                skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow']
-            },
-            {
-                img: 'assets/media/leadership/people/07-jay-oberay.png',
-                alt: 'Jay Oberay',
-                skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow', 'operation']
-            },
-            {
-                img: 'assets/media/leadership/people/08-dr-naama-kenan.png',
-                alt: 'Dr. Naama Kenan',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/09-kelvin-wu.png',
-                alt: 'Kelvin Wu',
-                skills: ['finance']
-            },
-            {
-                img: 'assets/media/leadership/people/10-leemor-machnai.png',
-                alt: 'Leemor Nachnai',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/11-merav-harel.png',
-                alt: 'Merav Harel',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/17-reut-dory.png',
-                alt: 'Reut Dory',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/13-monin-ung.png',
-                alt: 'Monin Ung',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/14-theekshana-kumara.png',
-                alt: 'Theekshana Kumara',
-                skills: ['finance', 'dealflow', 'taxation']
-            },
-            {
-                img: 'assets/media/leadership/people/15-or-dadosh.png',
-                alt: 'Or Dadosh',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/17-reut-dory.png',
-                alt: 'Reut Dory',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            },
-            {
-                img: 'assets/media/leadership/people/17-reut-dory.png',
-                alt: 'Reut Dory',
-                skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal']
-            }
+            { img: '17-reut-dory', alt: 'Reut Dory', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] },
+            { img: '02-asaf-eli', alt: 'Asaf Eli', width: 130, skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow'] },
+            { img: '03-asher-dory', alt: 'Asher Dory', width: 125, skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow'] },
+            { img: '04-eli-clark', alt: 'Eli Clark', skills: ['life-sciences', 'ip', 'bd', 'hr', 'dealflow'] },
+            { img: '05-gavin-cunningham', alt: 'Gavin Cunningham', skills: ['life-sciences', 'ip', 'bd', 'pm', 'dealflow', 'sales'] },
+            { img: '06-henry-chai', alt: 'Henry Chai', width: 120, skills: ['life-sciences', 'dealflow'] },
+            { img: '07-jay-oberay', alt: 'Jay Oberay', skills: ['life-sciences', 'ip', 'bd'] },
+            { img: '08-dr-naama-kenan', alt: 'Dr. Naama Kenan', skills: ['finance', 'dealflow', 'taxation', 'pm', 'bd', 'legal', 'pm', 'pm'] },
+            { img: '09-kelvin-wu', alt: 'Kelvin Wu', skills: ['finance'] },
+            { img: '10-leemor-machnai', alt: 'Leemor Nachnai', width: 175, skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal', 'legal'] },
+            { img: '11-merav-harel', alt: 'Merav Harel', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] },
+            { img: '12-michael-steven', alt: 'Michael Steven', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] },
+            { img: '13-monin-ung', alt: 'Monin Ung', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] },
+            { img: '14-theekshana-kumara', alt: 'Theekshana Kumara', skills: ['finance', 'dealflow', 'taxation'] },
+            { img: '15-or-dadosh', alt: 'Or Dadosh', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] },
+            { img: '16-andreas-athinodorou', alt: 'Andreas Athinodorou', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] },
+            { img: '17-reut-dory', alt: 'Reut Dory', skills: ['finance', 'dealflow', 'taxation', 'pm', 'technology', 'legal'] }
         ],
         skills: {
-            'legal': {
-                img: 'assets/media/leadership/skills/legal.svg',
-                alt: 'Legal',
-                color: 'C2C2C2',
-                width: null
-            },
-            'taxation': {
-                img: 'assets/media/leadership/skills/taxation.svg',
-                alt: 'Taxation',
-                color: '7FF88B',
-                width: null
-            },
-            'operation': {
-                img: 'assets/media/leadership/skills/operation.svg',
-                alt: 'Operation',
-                color: 'FBB786',
-                width: null
-            },
-            'technology': {
-                img: 'assets/media/leadership/skills/technology.svg',
-                alt: 'Technology',
-                color: 'A074FF',
-                width: 48
-            },
-            'life-sciences': {
-                img: 'assets/media/leadership/skills/life-sciences.svg',
-                alt: 'Life Sciences',
-                color: '77CEFF',
-                width: null
-            },
-            'ip': {
-                img: 'assets/media/leadership/skills/ip.svg',
-                alt: 'Intellectual Property',
-                color: 'FF6464',
-                width: null
-            },
-            'sales': {
-                img: 'assets/media/leadership/skills/sales.svg',
-                alt: 'Sales and Marketing',
-                color: 'FFF27B',
-                width: null
-            },
-            'bd': {
-                img: 'assets/media/leadership/skills/bd.svg',
-                alt: 'Business Development',
-                color: 'FF7CC3',
-                width: null
-            },
-            'pm': {
-                img: 'assets/media/leadership/skills/pm.svg',
-                alt: 'Product management and lifecycles',
-                color: 'BEED5A',
-                width: null
-            },
-            'hr': {
-                img: 'assets/media/leadership/skills/hr.svg',
-                alt: 'Profiling and Human Resources',
-                color: '59F3D8',
-                width: null
-            },
-            'dealflow': {
-                img: 'assets/media/leadership/skills/dealflow.svg',
-                alt: 'Network and Dealflow',
-                color: 'C3FFB5',
-                width: null
-            },
-            'finance': {
-                img: 'assets/media/leadership/skills/finance.svg',
-                alt: 'Finance and M&A',
-                color: '131313',
-                width: null
-            }
+            'legal': { img: 'legal', alt: 'Legal', color: 'C2C2C2', width: null },
+            'taxation': { img: 'taxation', alt: 'Taxation', color: '7FF88B', width: null },
+            'operation': { img: 'operation', alt: 'Operation', color: 'FBB786', width: null },
+            'technology': { img: 'technology', alt: 'Technology', color: 'A074FF', width: 48 },
+            'life-sciences': { img: 'life-sciences', alt: 'Life Sciences', color: '77CEFF', width: null },
+            'ip': { img: 'ip', alt: 'Intellectual Property', color: 'FF6464', width: null },
+            'sales': { img: 'sales', alt: 'Sales and Marketing', color: 'FFF27B', width: null },
+            'bd': { img: 'bd', alt: 'Business Development', color: 'FF7CC3', width: null },
+            'pm': { img: 'pm', alt: 'Product management and lifecycles', color: 'BEED5A', width: null },
+            'hr': { img: 'hr', alt: 'Profiling and Human Resources', color: '59F3D8', width: null },
+            'dealflow': { img: 'dealflow', alt: 'Network and Dealflow', color: 'C3FFB5', width: null },
+            'finance': { img: 'finance', alt: 'Finance and M&A', color: 'BDBDBD', width: null }
         }
     }
 
@@ -182,8 +55,16 @@ export class LeadershipSectionComponent implements AfterViewInit {
     }
 
     ngAfterViewInit(): void {
+        const peopleElements = this.people.nativeElement.children;
+        for (let i = 0, length = this.media.people.length; i < length; i++) {
+            const { width } = this.media.people[i];
+            if (width) {
+                this.renderer.setStyle(peopleElements[i].firstChild.children[1], 'width', `${width}px`);
+            }
+        }
+
         const skills = this.skillsTypes.nativeElement.children;
-        for (let i = 0, length= this.skillsArray.length; i < length; i++) {
+        for (let i = 0, length = this.skillsArray.length; i < length; i++) {
             const { width } = this.skillsArray[i];
             if (width) {
                 this.renderer.setStyle(skills[i].firstChild, 'width', `${width}px`);
--- client/src/app/components/skills-container/skills-container.component.ts
+++ client/src/app/components/skills-container/skills-container.component.ts
@@ -10,18 +10,36 @@ export class SkillsContainerComponent implements AfterViewInit {
     @Input() skills: string[];
     @Input() skillsIcons: { skill: { img: string, alt: string, color: string } };
     maxSkillsPerRow: number = 4;
-    slotSize: number = 45;
+    iconSize: number = 30;
+    margin: number = 9.6;
 
     constructor(private renderer: Renderer2) { }
 
     ngAfterViewInit(): void {
-        const skillsIcons = this.skillsContainer.nativeElement.children;
-        for (let i = 0, length = skillsIcons.length; i < length; i++) {
-            const element = skillsIcons[i];
-            const left = ((i % this.maxSkillsPerRow) * this.slotSize);
-            const top = (Math.floor(i / this.maxSkillsPerRow) * this.slotSize);
-            this.renderer.setStyle(element, 'top', `${top}px`);
-            this.renderer.setStyle(element, 'left', `${left}px`);
+        const skillsRows = this.skillsContainer.nativeElement.children;
+        for (let i = 0, length = this.skills.length; i < length; i++) {
+            const skill = this.skills[i];
+            if (i < 4) {
+                const div = this.renderer.createElement('div');
+                this.renderer.addClass(div, 'skill-icon');
+                const img = this.renderer.createElement('img');
+                this.renderer.setAttribute(img, 'src', `assets/media/leadership/skills/${this.skillsIcons[skill].img}.svg`)
+                this.renderer.setAttribute(img, 'alt', this.skillsIcons[skill].alt);
+                this.renderer.appendChild(div, img);
+                this.renderer.appendChild(skillsRows[0], div);
+            } else {
+                const div = this.renderer.createElement('div');
+                this.renderer.addClass(div, 'skill-icon');
+                const left = 9.6 + ((i % this.maxSkillsPerRow) * (this.iconSize + this.margin));
+                const top = 0;
+                this.renderer.setStyle(div, 'top', `${top}px`);
+                this.renderer.setStyle(div, 'left', `${left}px`);
+                const img = this.renderer.createElement('img');
+                this.renderer.setAttribute(img, 'src', `assets/media/leadership/skills/${this.skillsIcons[skill].img}.svg`)
+                this.renderer.setAttribute(img, 'alt', this.skillsIcons[skill].alt);
+                this.renderer.appendChild(div, img);
+                this.renderer.appendChild(skillsRows[1], div);
+            }
         }
     }
 }
