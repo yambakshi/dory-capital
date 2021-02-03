da79a3939451851246298fa51122326df5fbafb7
Yam Bakshi <yambakshi@gmail.com>
2021-02-03 13:09:59 +0200

    Made Intro Section Background Video Responsive

--- client/src/app/components/video-element/video-element.component.ts
+++ client/src/app/components/video-element/video-element.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, HostListener, Inject, Input, PLATFORM_ID, Renderer2, ViewChild } from '@angular/core';
+import { WindowRefService } from '@services/window-ref.service';
 
 
 @Component({
@@ -6,7 +8,7 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
     templateUrl: './video-element.component.html',
     styleUrls: ['./video-element.component.scss']
 })
-export class VideoElementComponent {
+export class VideoElementComponent implements AfterViewInit {
     @ViewChild('videoIframe') videoIframe: ElementRef;
     @Input() fullSize: boolean = true;
     iframeSrc: any = '';
@@ -25,17 +27,44 @@ export class VideoElementComponent {
         }
     }
 
-    constructor() {
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: any,
+        private renderer: Renderer2,
+        private windowRefService: WindowRefService) {
         const { url, params } = this.cloudinaryPlayer;
         const encodedParams = Object.entries(params)
-            .map(([param, value]) => {
-                if (Array.isArray(value)) {
-                    return encodeURIComponent(`${param}[0]`) + `=${value}`;
-                } else {
-                    return `${param}=${value}`;
-                }
-            }).join('&')
+            .map(([param, value]) =>
+                Array.isArray(value) ? encodeURIComponent(`${param}[0]`) + `=${value}` : `${param}=${value}`
+            ).join('&')
 
         this.iframeSrc = `${url}${encodedParams}`;
+    }
+
+    ngAfterViewInit(): void {
+        if (isPlatformBrowser(this.platformId)) {
+            this.calcPlayerSize();
+        }
+    }
+
+    calcPlayerSize(): void {
+        const ratio = 1280 / 720;
+        const windowHeight = this.windowRefService.nativeWindow.innerHeight;
+        const windowWidth = this.windowRefService.nativeWindow.innerWidth;
+        let width = 0, height = 0;
+        if (this.windowRefService.nativeWindow.innerWidth < 901) {
+            height = windowHeight;
+            width = height * ratio;
+        } else {
+            width = windowWidth;
+            height = width / ratio;
+        }
+
+        this.renderer.setStyle(this.videoIframe.nativeElement, 'width', `${width}px`);
+        this.renderer.setStyle(this.videoIframe.nativeElement, 'height', `${height}px`);
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event?) {
+        this.calcPlayerSize();
     }
 }
