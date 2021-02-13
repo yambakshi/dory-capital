1ab18c486d9b467b139ffd533ac3de4fdbe6811f
Yam Bakshi <yambakshi@gmail.com>
2021-02-13 17:36:35 +0200

    Improved READMEs And Fixed Minor Issue

--- server/app/services/auth/insert-user.ts
+++ server/app/services/auth/insert-user.ts
@@ -6,12 +6,12 @@ import bcrypt from 'bcrypt';
 export async function insertUser({ email, password }) {
     const user = await mongoDb.findOne(env.mongodb.dbName, 'users', { email });
     if (user) {
-        return Promise.reject('User already exist');
+        return Promise.reject({ message: 'User already exist' });
     }
 
     const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    const newUser = { email: email.toLowerCase(), password: hash };
-
+    const newUser = { email, password: hash };
     const output = mongoDb.insertMany(env.mongodb.dbName, 'users', [newUser]);
+
     return output;
 }
