7a288f2dff3ff421584215ba6e9b67ee2b4b7c5c
Yam Bakshi <yambakshi@gmail.com>
2021-02-26 19:07:46 +0200

    Implemented Components Lazy Loading

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomePageComponent } from '@components/home-page/home-page.component';
-import { AdminComponent } from '@components/admin/admin.component';
-import { LoginPageComponent } from '@components/login-page/login-page.component';
 import { AuthGuard } from './guards/auth.guard';
 import { PageDataResolver } from '@resolvers/page-data.resolver';
 
 
 const routes: Routes = [
   { path: '', component: HomePageComponent, resolve: { pageData: PageDataResolver } },
-  { path: 'login', component: LoginPageComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], resolve: { pageData: PageDataResolver } },
+  {
+    path: 'login',
+    loadChildren: () => import('@components/login-page/login-page.component').then(m => m.LoginPageComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('@components/admin/admin.component').then(m => m.AdminComponent),
+    canActivate: [AuthGuard], resolve: { pageData: PageDataResolver }
+  },
   { path: "**", redirectTo: "" }
 ];
 
