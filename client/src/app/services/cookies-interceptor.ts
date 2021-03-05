142091b8e27d7ce305e209eb0317b116ded30152
Yam Bakshi <yambakshi@gmail.com>
2021-03-05 04:13:10 +0200

    Cosmetic Fixes

--- client/src/app/services/cookies-interceptor.ts
+++ client/src/app/services/cookies-interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Inject, Injectable, Optional } from '@angular/core';
 import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { COOKIES } from './constants';
 
 @Injectable({
     providedIn: 'root'
@@ -10,20 +11,21 @@ export class CookiesInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         // If optional request is provided, we are server side
-        if (this.httpRequest) {
+        if (this.httpRequest && this.httpRequest.headers.cookie) {
             const cookies = this.httpRequest.headers.cookie;
             const cookiesObj = cookies.split('; ').reduce((acc, val) => {
                 const [k, v] = val.split('=');
                 return { ...acc, [k]: v };
             }, {});
 
-            const token = cookiesObj['dory_capital_token'];
+            const token = cookiesObj[COOKIES.PREFIX + COOKIES.TOKEN_KEY];
             req = req.clone({
                 setHeaders: {
                     Authorization: `bearer ${token}`
                 }
             });
         }
+
         return next.handle(req);
     }
 }
